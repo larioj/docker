ca3847189484c16e84e46dd0211b0348bd4a1a75
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2015-10-24 22:48:46 +0000

    teuh

--- vim-conf/vim/bide/runscripts/c_run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-fullname="$1"
-name="${fullname%.*}"
-outname="${name}.out"
-gcc $fullname -o $outname
-./$outname "${@:2}"
-rm $outname
--- vim-conf/vim/bide/runscripts/hs_run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-file=$1
-name="${file%.*}"
-ghc $file
-./$name "${@:2}"
-rm $name *.hi *.o
